0391c1f7f6b6212753eda65823c3f6d9215febe6
SooBinSuh <mysticraider2@gmail.com>
2025-05-09 19:23:29 +0900

    1차 완성

--- payload/experience.ts
+++ payload/experience.ts
@@ -5,45 +5,65 @@ const experience: IExperience.Payload = {
   disableTotalPeriod: false,
   list: [
     {
-      title: 'Linux Foundation',
+      title: '룩코',
       positions: [
         {
-          title: 'System Engineer',
-          startedAt: '1996-05',
+          title: '프런트엔드팀-앱개발',
+          startedAt: '2024-03',
+          endedAt: '2024-10',
           descriptions: [
-            'Steering Kernel patching and updates for various distributions',
-            'Automating deployment and integration processes for Linux servers',
-            'Spearheading initiatives to enhance Linux kernel security',
-            'Collaborating with global developers to coordinate code contributions',
+            '자사 앱 서비스 Secondsold 및 LookoAi 서비스의 다수 신규 화면 개발 및 기능 확장에 기여',
+
+            '자사 국내 모바일 앱 리브랜딩 작업 주도 (디자인 개편에 따른 주요 컴포넌트 전면 수정)',
+
+            'React Hook Form을 활용한 상품 필터 기능 개발',
+
+            'Firebase Remote Config 기반의 AB 테스트 기능 구현으로 앱 사용성 개선',
+
+            'Reanimated의 Worklet을 적용해 주요 화면의 애니메이션 성능 최적화',
+
+            'React Native 내 WebView를 활용한 커뮤니티 기능 설계 및 구현',
           ],
-          skillKeywords: ['C', 'Linux', 'Kernel', 'Security', 'Automation'],
+          skillKeywords: ['React Native', 'tanStack Query', 'react hook form', 'Reanimated'],
         },
         {
-          title: 'System Engineer Intern',
-          startedAt: '1996-02',
-          endedAt: '1996-05',
+          title: '프런트엔드팀-웹개발',
+          startedAt: '2024-03',
+          endedAt: '2024-10',
           descriptions: [
-            'Assisted senior engineers in maintaining and updating kernel patches for Linux distributions',
-            'Supported deployment and integration tasks for Linux server environments',
-            'Conducted research on Linux kernel security practices and presented findings to the team',
+            '자사 B2B 웹 솔루션 LookoAi 서비스 및 Secondsold 백오피스 웹 서비스 신규 기능 개발 및 운영 유지보수',
+            '웹 서비스의 다수 신규 화면 개발 및 기능 확장에 기여',
           ],
-          skillKeywords: ['C', 'Linux', 'Kernel', 'Support', 'Research'],
+          skillKeywords: ['React', 'vitejs', 'tanStack Query', 'react hook form'],
         },
       ],
     },
     {
-      title: 'Open Source Community',
+      title: '플러그링크',
       positions: [
         {
-          title: 'System Advocate and Developer',
-          startedAt: '2010-06',
-          endedAt: '2018-01',
+          title: 'iOS 네이티브 앱 개발',
+          startedAt: '2022-09',
+          endedAt: '2023-08',
           descriptions: [
-            'Promoted the use and development of Linux globally.',
-            'Developed automated scripts to streamline system configuration.',
-            'Facilitated live patching systems, reducing downtime.',
+            'React Native에서 네이티브 SwiftUI로 전환 개발 및 배포',
+            'BLE(Bluetooth Low Energy), iBeacon을 이용한 원격 충전 기능 개발',
+            '전기차 충전기 펌웨어 업데이트 변경사항 대응 (2주 1회)',
+            '푸시 알림 Deeplink를 통한 Nested 네비게이션 개발',
+            '23년 1월 서비스 전체 UI/UX 리브랜딩 진행',
           ],
-          skillKeywords: ['Open Source', 'Linux', 'AWS', 'RDBMS'],
+          skillKeywords: ['SwiftUI', 'Swift', 'MVVM'],
+        },
+        {
+          title: 'React Native 앱 개발',
+          startedAt: '2021-11',
+          endedAt: '2022-08',
+          descriptions: [
+            '자사 전기차 서비스 초기 API 검토 및 협력사 수정 요청',
+            'Firebase Crashlytics 사용하여 Crash Rate 5% 이하 상시 유지',
+            'CodePush를 통한 실시간 기능 배포',
+          ],
+          skillKeywords: ['ReactNative', 'Redux', 'CodePush', 'Typescript'],
         },
       ],
     },
--- payload/project.ts
+++ payload/project.ts
@@ -4,38 +4,26 @@ const project: IProject.Payload = {
   disable: false,
   list: [
     {
-      title: 'Linux Kernel Optimization Project',
-      startedAt: '2020-06',
-      where: 'Linux Foundation',
+      title: '생활알람 창업',
+      startedAt: '2020-11',
+      endedAt: '2021-10',
+      where: '4인 창업',
       descriptions: [
         {
-          content:
-            'Initiated and lead a collaborative project aimed at optimizing the Linux Kernel for various hardware architectures.',
-        },
-        {
-          content:
-            'Achieved significant improvements in system performance and resource management.',
-          weight: 'MEDIUM',
+          content: 'IOS Native 앱 개발(App Store 배포 후 철수)',
           descriptions: [
-            { content: '30% improvement in system resource efficiency' },
-            { content: 'Job Scheduler Refactor and Optimization' },
+            {
+              content:
+                '파편화된 정보를 수집하여 음성 알람으로 제공하고자 하는 서비스를 기획하여 기획, 디자인, 개발에 참여',
+              descriptions: [
+                { content: '코로나 확진자수, 대중교통 버스 및 지하철 현재 위치 정보' },
+              ],
+            },
+            {
+              content:
+                '앱 백그라운드 상태에서 백앤드 서버에서 데이터를 수신하여 클라이언트에서 TTS 음성알람 생성 및 재생, Local Push Notification 기능 구현',
+            },
           ],
-        },
-      ],
-    },
-    {
-      title: 'Global Linux Bootcamp',
-      startedAt: '2017-05',
-      endedAt: '2019-12',
-      where: 'Open Source Community',
-      descriptions: [
-        {
-          content:
-            'Organized and conducted training bootcamps focusing on Linux system engineering skills.',
-        },
-        {
-          content:
-            'Successfully trained over 5,000 participants in system administration and security. ',
         },
       ],
     },
